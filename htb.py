1eafc16b26faf51b02fbc1e46394678f50990fe7
MrDoiman <52940085+MrDoiman@users.noreply.github.com>
2022-03-30 10:44:35 +0200

    Upload Files

---
+++ htb.py
@@ -0,0 +1,59 @@
+from pypresence import Presence
+import time
+import re, sys, subprocess
+
+maquina = input("Machine name: ")
+
+if len(sys.argv) != 2:
+    print("\n[!] Uso: python3 " + sys.argv[0] + " <ip_address>\n")
+    sys.exit(1)
+
+def get_ttl(ip_address):
+
+    proc = subprocess.Popen(["/sbin/ping -c 1 %s" % ip_address, ""], stdout=subprocess.PIPE, shell=True)
+    (out,err) = proc.communicate()
+
+    out = out.split()
+    out = out[12].decode('utf-8')
+
+    ttl_value = re.findall(r"\d{1,3}", out)[0]
+
+    return ttl_value
+
+def get_os(ttl):
+
+    ttl = int(ttl)
+
+    if ttl >= 0 and ttl <= 64:
+        return "Linux"
+    elif ttl >= 65 and ttl <= 128:
+        return "Windows"
+    else:
+        return "Not Found"
+
+if __name__ == '__main__':
+
+    ip_address = sys.argv[1]
+
+    ttl = get_ttl(ip_address)
+
+    os_name = get_os(ttl)
+    print("%s (ttl -> %s): %s" % (ip_address, ttl, os_name))
+
+client_id = 'CLIENT_ID_GOES_HERE'
+RPC = Presence(client_id)
+RPC.connect()
+
+if os_name == "Linux":
+    imagen = "linux"
+elif os_name == "Windows":
+    imagen = "windows"
+else:
+    print("Error: OS not recognized.")
+    exit()
+
+RPC.update(state="Hacking The Box...", details="/dev/tcp/" + ip_address, large_image="htb", small_image=imagen, large_text="HTB: " + maquina, small_text=os_name, start=time.time())
+print("Discord Connection Success! (Don't end the script until the machine is finished)")
+
+while True:
+    time.sleep(15)
